1f3d6a8212a6cae6793df2ed35639aa518c6e56b
Noel Soong <noelsoong@live.com>
2019-06-26 11:03:31 +1200

    Sever uses CORS as middlewares

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 
 const server = express()
 
+server.use(cors())
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
