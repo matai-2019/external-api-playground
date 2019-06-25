d7c4dab7a6eebcaada5f98e21c2a175b880d85d5
Tamari Jacob Gray-Kaiwahia <tamarigray97@gmail.com>
2019-06-26 11:23:41 +1200

    frontend ready

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+export default class App extends Component {
+  state = {
+    people: [1, 2]
+  }
+
+  componentDidMount () {
+    // make api call
+  }
+  render () {
+    return (
+      <>
+        <h1 style={{ textAlign: 'center' }}>DEVELOCONS</h1>
+        <div className="people-list">
+          {
+            this.state.people.map(person => {
+              return <div key={person} className="person">
+                <h3>{person.name}</h3>
+                <div style={{ position: 'relative' }}>
+                  <img src={person.img} className={{ 'display-img': true, 'img': true }} alt=""/>
+                  <img src={person.robot} className={{ 'display-img': true, 'robot': true }} alt=""/>
+                </div>
+                <h2>hello</h2>
+              </div>
+            })
+          }
+        </div>
+      </>
+    )
+  }
 }
-
-export default App
