2bb173f217d056e3a6a5adf4ac9aa7649a0033b7
David Nicholson <davidnicholsonjob@gmail.com>
2019-06-26 11:40:15 +1200

    random numbner

---
+++ client/api.js
@@ -0,0 +1,5 @@
+import request from 'superagent'
+import numberURL from 'http://numbersapi.com/'
+
+export function getNumber (callback)
+
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+// import api from '../api'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      number: this.randomNumber(300)
+    }
+  }
+
+  randomNumber (num) {
+    return Math.floor(Math.random() * num)
+  }
+
+  componentDidMount () {
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Number Facts</h1>
+        <div className='Number' > {this.state.number} </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
