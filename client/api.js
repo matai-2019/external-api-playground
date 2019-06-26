e883495818d9436d3d765323d4b3dacf05134ddf
Jennie Watson <watson.jennie17@gmail.com>
2019-06-26 12:10:24 +1200

    Nasa Pic of the Day api hide key

--- client/api.js
+++ client/api.js
@@ -3,8 +3,6 @@ import keys from '../keys'
 
 const nasaUrl = `https://api.nasa.gov/planetary/apod?${keys.nasaKey}`
 
-// const nasaUrl = 'https://api.nasa.gov/planetary/apod?api_key=OgnSEJ0P2BEKUoi48lXMeAgPhXMLXRdjARuf11aZ'
-
 export function picOfADay () {
   return request.get(nasaUrl)
     .then(res => {
