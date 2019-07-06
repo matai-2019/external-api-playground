5f6f60286550b20f76d47536bf807367cfe3d449
Jennie Watson <watson.jennie17@gmail.com>
2019-07-06 17:31:24 +1200

    trying to display character deets

--- client/api.js
+++ client/api.js
@@ -1,6 +1,5 @@
 import request from 'superagent'
 const hpUrl = 'https://www.potterapi.com/v1/characters?key=$2a$10$ZbeMuQMa5WtxQ4XOtqwywOFe.nwFFXu27Z3egbPP5gz59aIuvIWAS'
-const url = 'https://www.potterapi.com/v1/characters/5a0fa4daae5bc100213c232e?key=$2a$10$ZbeMuQMa5WtxQ4XOtqwywOFe.nwFFXu27Z3egbPP5gz59aIuvIWAS'
 
 export function getCharacters () {
   return request
@@ -12,10 +11,17 @@ export function getCharacters () {
     })
 }
 
-export function getCharDetails () {
-  for (let i = 0; i < res.body.length; i++) {
-    let id = res.body[i].id
-  }
+// export function getCharDetails (req) {
+//   const url = `https://www.potterapi.com/v1/characters/${req.params.id}?key=$2a$10$ZbeMuQMa5WtxQ4XOtqwywOFe.nwFFXu27Z3egbPP5gz59aIuvIWAS`
+//   return request
+//     .get(url)
+//     .then(res => {
+//       console.log(res.body)
+//       return res.body
+//     })
+// }
+
+export function getCharDetails (id) {
   const url = `https://www.potterapi.com/v1/characters/${id}?key=$2a$10$ZbeMuQMa5WtxQ4XOtqwywOFe.nwFFXu27Z3egbPP5gz59aIuvIWAS`
   return request
     .get(url)
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 // import api from '../api'
 import { getCharacters, getCharDetails } from '../api'
-// import { Link, Route } from 'react-router-dom'
 // import Character from './Character'
 
 class App extends React.Component {
@@ -21,38 +20,35 @@ class App extends React.Component {
   }
 
   displayCharDetails = () => {
-    // const details = this.state.characters[0]
-    // this.setState({
-    //   details
-    // })
-    getCharDetails()
-      .then(details => {
-        this.setState({
-          details: details
+    for (let i = 0; i < this.state.characters.length; i++) {
+      return getCharDetails(i.id)
+        .then(details => {
+          this.setState({
+            details: details
+          })
         })
-      })
+    }
   }
 
-  render () {
+  render() {
     const hpPeople = this.state.characters
     return (
       <>
-      <h1>Harry Potter API Practice</h1>
-      <button onClick={this.handleClick}>Load all HP characters!</button>
-      <ul>
-        {hpPeople.map(e => {
-          return <li key={e.id}><a href="#" onClick={getCharDetails}>{e.name}</a></li>
-        })}
-      </ul>
-      <button onClick={this.displayCharDetails}>Details</button>
-      <ul>
-        <li>{this.state.details.name}</li>
-        <li>{this.state.details.house}</li>
-        <li>{this.state.details.school}</li>
-        <li>{this.state.details.species}</li>
-        <li>{this.state.details.bloodStatus}</li>
-        <li>{this.state.details.role}</li>
-      </ul>
+        <h1>Harry Potter API Practice</h1>
+        <button onClick={this.handleClick}>Load all HP characters!</button>
+        <ul>
+          {hpPeople.map(e => {
+            return <li key={e._id}><button onClick={this.displayCharDetails}>{e.name}</button></li>
+          })}
+        </ul>
+        <ul>
+          <li>{this.state.details.name}</li>
+          <li>{this.state.details.house}</li>
+          <li>{this.state.details.school}</li>
+          <li>{this.state.details.species}</li>
+          <li>{this.state.details.bloodStatus}</li>
+          <li>{this.state.details.role}</li>
+        </ul>
       </>
     )
   }
