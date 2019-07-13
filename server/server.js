5162251198911f999c9b7611a5574148ee21e405
Jared <jared@needapart.co.nz>
2019-07-13 18:44:22 +1200

    Connect routes to the server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const path = require('path')
 const express = require('express')
 
+const router = require('./routes')
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+server.use('/astro', router)
 
 module.exports = server
