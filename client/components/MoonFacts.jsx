00f1d0fdd625243abb8fd3ab08668a81734f11bc
Jared <jared@needapart.co.nz>
2019-07-14 13:06:26 +1200

    Fill out MoonFacts with details from the API

--- client/components/MoonFacts.jsx
+++ client/components/MoonFacts.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Statistic, Card, Icon, Header, Image } from 'semantic-ui-react';
 
 const MoonFacts = ({moonFacts}) => {
+  const items = [
+    {key: 'rise', label: 'rise', value: moonFacts.moonrise},
+    {key: 'set', label: 'set', value: moonFacts.moonset},
+    {key: 'distance', label: 'distance (km)', value: Math.floor(moonFacts.moon_distance)}
+  ]
 return (
-  <>
-  <p>The moons distance from you is {moonFacts.moon_distance}</p>
-  </>
-)
+  <Card fluid>
+    <Image src='/images/moon.jpg' wrapped ui={false} />
+    <Card.Content>
+      <Header as='h3'><Icon name='moon outline' size='big' color='yellow'/>Moon Facts</Header>
+      <Statistic.Group items={items} />
+    </Card.Content>
+  </Card>
+  )
 }
 
 export default MoonFacts
