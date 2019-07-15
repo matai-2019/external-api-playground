881ebba29fab2e9135a287a497237fd9b2301d1f
Taine Walford <tainewalford@live.com>
2019-07-15 21:04:32 +1200

    moved auth token to .env file

--- server/routes/serverApi.js
+++ server/routes/serverApi.js
@@ -1,40 +1,34 @@
+require('dotenv').config()
 const fs = require('fs')
 const request = require('superagent')
 const express = require('express')
 const router = express.Router()
 const idUrl = 'https://www.slack.com/api/channels.info?channel=CJX8XUQLE'
 const usersUrl = 'https://slack.com/api/users.list?team=T02SQPVAC'
-const auth = 'xoxp-2908811352-589200241107-670392999473-621348811b77f4d77e03fb0977432ef0'
 let matai = []
 let all = []
-
 require.extensions['.txt'] = function (module, filename) {
-  module.exports = fs.readFileSync(filename, 'utf8');
-};
+  module.exports = fs.readFileSync(filename, 'utf8')
+}
 
-const token = require('../../auth.txt').slice(1)
-
-console.log('auth: ', token)
+const auth = process.env.API_KEY
 
 router.get('/', (req, res) => {
-  console.log('slack home hit |', auth)
   request
     .get(idUrl)
     .set({
-      'Authorization': 'Bearer ' + auth
+      Authorization: 'Bearer ' + auth
     })
     .then(channelRes => {
-      console.log('channel url')
       matai = channelRes.body.channel.members
       request
         .get(usersUrl)
         .set({
-          'Authorization': 'Bearer ' + auth
+          Authorization: 'Bearer ' + auth
         })
         .then(userRes => {
-          console.log('user url')
           all = userRes.body.members
-          let mataiNames = []
+          const mataiNames = []
           matai.map(memberID => {
             all.forEach(member => {
               if (member.id === memberID) mataiNames.push(member.real_name)
